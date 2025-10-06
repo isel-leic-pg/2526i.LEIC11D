66a1ea266656033e8c3c7a5416b4d9aa6b930426
Pedro Pereira <593033+palex65@users.noreply.github.com>
2025-10-06 13:31:31 +0100

    Aula 6Out
    Ciclos e Listas

--- SandBox/src/main/kotlin/Unicode.kt
+++ SandBox/src/main/kotlin/Unicode.kt
@@ -1,10 +1,17 @@
 
+
 fun main() {
-    for (c in '0'..'9')
+    val start = readChar("Start char")
+    val end = readChar("End char", start..'Z')
+    for( c in start..end)
         println("$c - ${c.code}")
+}
 
-    for(c in "ISEL") println(c)
-
-    for(s in listOf("ISEL","PG","IPL"))
-        println(s)
+fun readChar(quest: String, valid: CharRange = 'A'..'Z'): Char {
+    while(true) {
+        print("$quest ? ")
+        val c = readln().trim()[0]
+        if (c in valid) return c
+        println("Invalid $c")
+    }
 }
